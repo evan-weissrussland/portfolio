8a4731d25a304037c57940501ba1b52057dbfe84
“evan-weissrussland” <evanwodichka@gmail.com>
2024-03-31 17:33:01 +0300

    ad library typewriter

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,15 +3,29 @@ import iconsHome from "../images/iconsHome.svg";
 import backHomeImage from "../images/backHome1.webp";
 import {Button} from "./assets/Button";
 import {Container} from "./assets/Container";
+import Typewriter from 'typewriter-effect';
 
 export const Home = () => {
     return (
         <HomeSection id={'home'}>
             <Container>
                 <HomeWrapper>
-                    <p>Hello, I'm <span>Lucas Alves</span></p>
-                    <h1>Graphic Designer & Ceo of Graphic Type.
-                        I love to provide quality works.
+                    <article>
+                        <Typewriter
+                            options={{
+                                autoStart: true,
+                                delay: 70,
+                                loop:true,
+                                cursor: '',
+                            }}
+                            onInit={(typewriter) => {
+                                typewriter.typeString(`<p>Hello, I'm <p> <span>Lucas Alves</span>`)
+                                    .start();
+                            }}
+                        />
+                    </article>
+                    <h1>
+                        Graphic Designer & Ceo of Graphic Type. I love to provide quality works.
                     </h1>
                     <Button $borderRadius={'30px'}
                             $border={'1px solid #fba820'}
@@ -28,8 +42,6 @@ export const Home = () => {
     )
 }
 
-
-
 export const HomeWrapper = styled.div`
     max-width: 350px;
     width: 100%;
@@ -40,15 +52,21 @@ export const HomeWrapper = styled.div`
     h1 {
         font-size: 18px;
         margin-bottom: 54px;
+        //height: 41px;
     }
 
     ${Button}:hover {
         background: rgba(251, 168, 32, 0.15);
     }
 
-    p {
+    & > article {
         font-size: 70px;
         margin-bottom: 15px;        
+        height: 166px;
+
+        p {
+            color: white;  
+        }
         
         span {
             color: #fba820;
@@ -62,10 +80,12 @@ export const HomeWrapper = styled.div`
         background: url(${iconsHome}) no-repeat;
     }
 `
- const HomeSection = styled.section`
-    background: black  0 url(${backHomeImage}) no-repeat;
-    background-position: center ; 
-     @media (max-width: 999px) {
-         background-position: 58% center;
-     }
+
+const HomeSection = styled.section`
+    background: black 0 url(${backHomeImage}) no-repeat;
+    background-position: center;
+    
+    @media (max-width: 999px) {
+        background-position: 58% center;
+    }
 `
